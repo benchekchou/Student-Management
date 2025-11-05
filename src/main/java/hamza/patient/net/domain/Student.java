686dba9ff5c8490e4bfc5f320e57fc4cbd5f5a9b
assiasouitat <assia.souitat@gmail.com>
2025-11-05 12:19:23 +0100

    kan-8:add Student entity with attributes and basic methods

--- src/main/java/hamza/patient/net/domain/Student.java
+++ src/main/java/hamza/patient/net/domain/Student.java
@@ -2,6 +2,7 @@ package hamza.patient.net.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Student {
     private String id;
@@ -19,6 +20,10 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+
+    public Student(String id) {
+        this.id = id;
     }
 
     public void setName(String name) {
@@ -41,6 +46,25 @@ public class Student {
         this.notes = notes;
     }
     // getters/setters/constructeurs
-    // equals/hashCode/toString
+    // equals/hashCode/toStrin
 
+    public Student(String id, String name, String email, List<CourseNote> notes) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.notes = notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(notes, student.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, notes);
+    }
 }
