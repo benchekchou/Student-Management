2e59dca53e2ab554432dbcc730adeeb89979398a
assiasouitat <assia.souitat@gmail.com>
2025-11-05 11:44:42 +0100

    kan-8:add Student entity with attributes and basic methods

--- src/main/java/hamza/patient/net/domain/Student.java
+++ src/main/java/hamza/patient/net/domain/Student.java
@@ -9,6 +9,37 @@ public class Student {
     private String email;
     private List<CourseNote> notes = new ArrayList<>();
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<CourseNote> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<CourseNote> notes) {
+        this.notes = notes;
+    }
     // getters/setters/constructeurs
     // equals/hashCode/toString
 
